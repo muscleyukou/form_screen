a71237f2de29ec7fd8516eb5b6926f4c7e807d24
hayashiyuuatsu <yukou29good0910@gmail.com>
2020-05-07 15:13:29 +0900

    #2

--- lib/screens/confirm_screen.dart
+++ lib/screens/confirm_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutterapp/parts/button_text.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 class ConfirmScreen extends StatefulWidget {
   @override
@@ -7,11 +11,34 @@ class ConfirmScreen extends StatefulWidget {
   final mail;
   final gender;
   final questionFirst;
-  final  questionSecond;
-  ConfirmScreen({@required this.userName,this.mail,this.gender,this.questionFirst,this.questionSecond});
+  final questionSecond;
+
+  ConfirmScreen(
+      {@required this.userName,
+      this.mail,
+      this.gender,
+      this.questionFirst,
+      this.questionSecond});
 }
 
 class _ConfirmScreenState extends State<ConfirmScreen> {
+  String formResponse='';
+
+  Future send() async {
+    final Email email = Email(
+      body: _resultQuestion().toString(),
+      recipients: ['yukou29good0910@gmail.com'],
+      cc: ['yukoufitness.0910@gmail.com'],
+      isHTML: false,
+    );
+    try {
+      await FlutterEmailSender.send(email);
+      formResponse = 'success';
+    } catch (e) {
+      formResponse = e.toString();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,10 +50,82 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
         centerTitle: true,
         backgroundColor: Colors.cyanAccent,
       ),
-   body: Text(widget.mail.toString()),
-
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 80.0,
+                ),
+                Container(
+                  color: Colors.cyanAccent,
+                  height: 400.0,
+                  width: 350.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'こちらの内容でよろしいでしょうか？',
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text('Name：${widget.userName.toString()}'),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text('Mail：${widget.mail.toString()}'),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text('Gender：${widget.gender}'),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text('質問の回答内容'),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text('質問１：筋トレは好きですか？？'),
+                      Text('回答１：${widget.questionFirst}'),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text('質問2：筋トレはどのくらいの頻度で\n行なっていますか？？'),
+                      Text('回答2：${widget.questionSecond}'),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                Text('宜ければ送信ボタンを押してください'),
+                SizedBox(
+                  height: 8.0,
+                ),
+                ButtonInText(
+                    function: send,
+                    color: Colors.black,
+                    backgroundColor: Colors.white,
+                    text: '送信する'),
+                Text("送信結果:${formResponse}"),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
-
   }
 
+  _resultQuestion() {
+    return '${widget.userName.toString()}'
+        '\n${widget.mail.toString()}'
+        '\n${widget.gender.toString()}'
+        '\n回答１：${widget.questionFirst}'
+        '\n回答2：${widget.questionSecond}';
+  }
 }
